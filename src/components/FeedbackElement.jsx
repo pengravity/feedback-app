a0b0d6cae251fa9a8686ac6cebedd8878662d307
Andy <alipinski@mail.com>
2022-09-04 20:17:02 +0200

    added option to delete list elements

--- src/components/FeedbackElement.jsx
+++ src/components/FeedbackElement.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useState } from 'react';
+import Card from './shared/Card';
+import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
 
-function FeedbackElement({ element }) {
+function FeedbackElement({ element, handleDelete }) {
   return (
-    <div className='card'>
+    <Card reverse={true}>
       <div className='num-display'>{element.rating}</div>
+      <button onClick={() => handleDelete(element.id)} className='close'>
+        <FaTimes color='white' />
+      </button>
       <div className='text-display'>{element.text}</div>
-    </div>
+    </Card>
   );
 }
+
+FeedbackElement.propTypes = {
+  element: PropTypes.object.isRequired,
+};
 
 export default FeedbackElement;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import FeedbackElement from './FeedbackElement';
 
-function FeedbackList({ feedback }) {
+import FeedbackElement from './FeedbackElement';
+import PropTypes from 'prop-types';
+
+function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>no feedback yet</p>;
   }
   return (
     <div className='feedback-list'>
       {feedback.map((element) => (
-        <FeedbackElement key={element.id} element={element} />
+        <FeedbackElement
+          key={element.id}
+          element={element}
+          handleDelete={handleDelete}
+        />
       ))}
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
