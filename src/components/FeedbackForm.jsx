4787ccb4b884b35e9d6d856f467b8d619ac9b638
Andy <alipinski@mail.com>
2022-09-05 13:52:02 +0200

    added Button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './shared/Card';
+import Button from './shared/Button';
 
 import { useState } from 'react';
 
@@ -22,7 +23,7 @@ function FeedbackForm() {
             placeholder='write a review'
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
