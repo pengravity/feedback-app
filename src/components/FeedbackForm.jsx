14a897ee3a6e22e8b773e6f21eb55ac78238aef5
Andy <alipinski@mail.com>
2022-09-05 15:20:28 +0200

    added functionality to the feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,8 +6,21 @@ import { useState } from 'react';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (event) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length < 11) {
+      setBtnDisabled(true);
+      setMessage('review need to be at least 10 letters');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(event.target.value);
   };
 
@@ -23,8 +36,11 @@ function FeedbackForm() {
             placeholder='write a review'
             value={text}
           />
-          <Button type='submit'>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
