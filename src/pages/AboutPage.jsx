d95ad1bb0745678c8a37dc423c29815499fc00a0
Andy <alipinski@mail.com>
2022-09-06 14:29:28 +0200

    adding react-router-dom and about page

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className='about'>
+        <h2>About project</h2>
+        <p> This is a React app to leave rating and feedback for something</p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
